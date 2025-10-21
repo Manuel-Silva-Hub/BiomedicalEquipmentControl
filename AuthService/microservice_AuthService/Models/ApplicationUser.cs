448946d671c86e76d37208b788a973f5472388fa
Manuel-Silva-Hub <alexissilva.22ruiz@gmail.com>
2025-10-21 11:06:48 -0500

    Implementation of the first AuthService microservice

---
+++ AuthService/microservice_AuthService/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace microservice_AuthService.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string RolHospital { get; set; } // Ej: "Administrador", "Técnico", etc.
+    }
+}
---
+++ AuthService/microservice_AuthService/Models/JwtTokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace microservice_AuthService.Models
+{
+    public class JwtTokenResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
