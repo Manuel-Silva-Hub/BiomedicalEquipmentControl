e448f2dbd1c8131e5ddcbeae37d32cbf597695b4
Manuel-Silva-Hub <alexissilva.22ruiz@gmail.com>
2025-10-24 09:20:55 -0500

    First version of the Equipment Service microservice

---
+++ EquipmentService/microservice_Equipment/Data/RegistroContext.cs
@@ -0,0 +1,14 @@
+﻿using microservice_Equipment.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using microservice_Equipment.Models;
+
+namespace microservice_Equipment.Data
+{
+    public class RegistroContext : DbContext
+    {
+        public RegistroContext(DbContextOptions<RegistroContext> options) : base(options) { }
+
+        public DbSet<EquipmentRegistration> Equipmentregistration { get; set; }
+    }
+}
---
+++ EquipmentService/microservice_Equipment/Models/EquipmentRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace microservice_Equipment.Models
+{
+    public class EquipmentRegistration
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string EquipmentType { get; set; } // Technological or Biomedical
+
+        [Required]
+        public string Serial { get; set; }
+
+        public string Description { get; set; }
+
+        public string QRCode { get; set; } // Optional for frequent teams
+        public string PhotoUrl { get; set; }
+
+        [Required]
+        public DateTime EntryDate { get; set; } = DateTime.Now;
+
+        public DateTime? OutDate { get; set; }
+
+        [Required]
+        public string LoginUser { get; set; }
+
+        public string? OutUser { get; set; }
+
+        public bool IsFrequent { get; set; } = false;
+        public bool IsInside { get; set; } = true;
+    }
+}
